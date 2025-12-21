074c7c81ef27c6bd08eb7ed56111fac9a68d27d6
Tim Rush <timbone72@gmail.com>
2025-12-21 05:26:59 +0000

    File changes

--- src/pages/JobKits.jsx
+++ src/pages/JobKits.jsx
@@ -275,6 +275,7 @@ export default function JobKits() {
           </DialogContent>
         </Dialog>
       </div>
+    </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {kits?.map((kit) => (
